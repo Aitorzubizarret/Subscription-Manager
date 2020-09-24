5af4df8712a9052bd92e0c48add257ad97edb536
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2020-09-24 23:06:27 +0200

    Initial Commit

---
+++ SubscriptionManager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SubscriptionManager
+//
+//  Created by Aitor Zubizarreta on 24/09/2020.
+//  Copyright © 2020 Aitor Zubizarreta. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
