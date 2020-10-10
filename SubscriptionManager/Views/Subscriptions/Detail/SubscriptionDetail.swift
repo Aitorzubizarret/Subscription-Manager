06a77f4cde60c3362ccdeefdf53fe178164bda6f
Aitor <aitorzubizarreta@yahoo.es>
2020-10-10 19:24:13 +0200

    Updated the 'SubscriptionDetail' controller to show the info of the subscription and added the button to delete it.

--- SubscriptionManager/Views/Subscriptions/Detail/SubscriptionDetail.swift
+++ SubscriptionManager/Views/Subscriptions/Detail/SubscriptionDetail.swift
@@ -9,12 +9,72 @@
 import SwiftUI
 
 struct SubscriptionDetail: View {
+    
+    //MARK: - Properties
+    @Environment(\.managedObjectContext) var moc
+    @State private var showingAlert: Bool = false
     var subscription: Subscription
     
+    //MARK: - Methods
+    ///
+    /// Deletes the subscription.
+    ///
+    private func deleteSubscription() {
+        self.moc.delete(self.subscription)
+        
+        do {
+            try self.moc.save()
+        } catch {
+            print("Error deleting a subscription: \(error)")
+        }
+    }
+    
+    //MARK: - View
     var body: some View {
         return VStack {
-            Text(subscription.name)
-            Text("\(subscription.price)")
+            Image(systemName: "info.circle")
+                .resizable()
+                .frame(width: 75, height: 75, alignment: .center)
+                .scaledToFit()
+                .padding(EdgeInsets(top: 0, leading: 0, bottom: 20, trailing: 0))
+                .foregroundColor(Color.red)
+            HStack {
+                Text("Name :")
+                    .padding(EdgeInsets(top: 10, leading: 20, bottom: 0, trailing: 0))
+                Spacer()
+                Text(subscription.name)
+                    .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 20))
+            }
+            HStack {
+                Text("Price :")
+                    .padding(EdgeInsets(top: 10, leading: 20, bottom: 0, trailing: 0))
+                Spacer()
+                Text(String(format: "%.2f €", subscription.price))
+                .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 20))
+            }
+            Spacer()
+            Button(action: {
+                self.showingAlert = true
+            }) {
+                HStack {
+                    Spacer()
+                    Image(systemName: "trash")
+                        .padding(EdgeInsets(top: -3, leading: 0, bottom: 0, trailing: 0))
+                    Text("Delete Subscription")
+                        .padding(EdgeInsets(top: 3, leading: 0, bottom: 0, trailing: 0))
+                    Spacer()
+                }
+                .padding()
+                .background(Color.red)
+                .foregroundColor(Color.white)
+                .font(.headline)
+                .cornerRadius(6)
+                .padding(EdgeInsets(top: 0, leading: 10, bottom: 10, trailing: 10))
+            }.alert(isPresented: self.$showingAlert, content: {
+                Alert(title: Text("Delete Subscription"), message: Text("This action can’t be undone. Are you sure?") ,primaryButton: Alert.Button.destructive(Text("Delete"), action: {
+                    self.deleteSubscription()
+                }), secondaryButton: Alert.Button.cancel(Text("No")))
+            })
         }
     }
 }
