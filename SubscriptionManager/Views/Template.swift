5b8da1f1e8b8424e93e45401634f81cfe81392a9
Aitor <aitorzubizarreta@yahoo.es>
2020-10-21 23:46:02 +0200

    Modified a custom color from 'Template' and added 3 more.

--- SubscriptionManager/Views/Template.swift
+++ SubscriptionManager/Views/Template.swift
@@ -12,5 +12,8 @@ extension Color {
     static let customLightGrey = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
     static let customDarkGrey = Color(red: 218.0/255.0, green: 218.0/255.0, blue: 218.0/255.0, opacity: 1.0)
     static let customDarkText = Color(red: 137.0/255.0, green: 137.0/255.0, blue: 137.0/255.0, opacity: 1.0)
-    static let customRedButton = Color(red: 255.0/255.0, green: 73.0/255.0, blue: 73.0/255.0, opacity: 1.0)
+    static let customRedText = Color(red: 255.0/255.0, green: 83.0/255.0, blue: 58.0/255.0, opacity: 1.0)
+    static let customRedButton = Color(red: 250.0/255.0, green: 240.0/255.0, blue: 240.0/255.0, opacity: 1.0)
+    static let customBlueText = Color(red: 42.0/255.0, green: 124.0/255.0, blue: 255.0/255.0, opacity: 1.0)
+    static let customBlueButton = Color(red: 237.0/255.0, green: 242.0/255.0, blue: 252.0/255.0, opacity: 1.0)
 }
