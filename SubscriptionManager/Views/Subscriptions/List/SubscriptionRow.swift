fede24e31afab3745c4d74bfb78bf688b18e1f46
Aitor <aitorzubizarreta@yahoo.es>
2020-11-17 23:43:02 +0100

    Added a new method 'compareTodayDateAndPaymentDate' in 'SubscriptionRow' to compare today's date with the payment date. If the difference is 10 days or less it shows a label.

--- SubscriptionManager/Views/Subscriptions/List/SubscriptionRow.swift
+++ SubscriptionManager/Views/Subscriptions/List/SubscriptionRow.swift
@@ -19,9 +19,14 @@ struct SubscriptionRow: View {
         let finalPrice: String = String(format: "%.2f €", subscriptionPrice)
         return finalPrice
     }
+    private var daysToPaymentText: String = ""
+    private var paymentDayNear: Bool = false
     private var textColor: Color = Color.clear
     private var backgroundColor: Color = Color.clear
     private var strokeColor: Color = Color.clear
+    private var widthValue: CGFloat {
+        return (UIScreen.main.bounds.size.width - 15 - 15) // SubscriptionView, leading and trailing padding.
+    }
     
     //MARK: - Methods
     
@@ -29,6 +34,8 @@ struct SubscriptionRow: View {
         self.subscription = subscription
         
         self.configureUIElements()
+        
+        self.compareTodayDateAndPaymentDate()
     }
     
     ///
@@ -40,6 +47,54 @@ struct SubscriptionRow: View {
         self.strokeColor = Color.customRowPistachio
     }
     
+    ///
+    /// Compares today's date and the payment date, and displays a label if the date is 10 days or less apart.
+    ///
+    private mutating func compareTodayDateAndPaymentDate() {
+        
+        let paymentDate: Date = self.subscription.nextPayment
+        
+        // Creates today's date. Ex. 2020/04/25 00:00:00
+        var todayComponents: DateComponents = DateComponents()
+        todayComponents.year = Date().getYearNumber()
+        todayComponents.month = Date().getMonthNumber()
+        todayComponents.day = Date().getDayNumber()
+        todayComponents.hour = 1
+        todayComponents.minute = 0
+        todayComponents.second = 0
+        let today: Date = Calendar.current.date(from: todayComponents) ?? Date()
+        
+        // Create the date, Today plus 10 more days.
+        var dayComponent = DateComponents()
+        dayComponent.day = 10
+        let dateInTenDays: Date? = Calendar.current.date(byAdding: dayComponent, to: today)
+        
+        if let finalDate: Date = dateInTenDays {
+            if paymentDate > finalDate {
+                self.paymentDayNear = false
+            } else {
+                let component = Calendar.current.dateComponents([.day], from: today, to: paymentDate)
+                if let daysLeft: Int = component.day {
+                    switch daysLeft {
+                    case 0 :
+                        self.daysToPaymentText = "Today"
+                    case 1:
+                        self.daysToPaymentText = "Tomorrow"
+                    case 2, 3, 4, 5, 6, 8, 9, 10 :
+                        self.daysToPaymentText = "In \(daysLeft) days"
+                    case 7:
+                        self.daysToPaymentText = "In 1 week"
+                    default:
+                        self.daysToPaymentText = ""
+                    }
+                    
+                }
+                self.paymentDayNear = true
+            }
+        } else {
+            self.paymentDayNear = false
+        }
+    }
     //MARK: - View
     
     var body: some View {
@@ -51,15 +106,31 @@ struct SubscriptionRow: View {
                     .scaledToFit()
                     .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
                     .foregroundColor(self.textColor)
-                Text(subscription.name).font(Font.system(size: 18))
+                Text(subscription.name)
+                    .font(Font.system(size: 18, weight: Font.Weight.bold, design: Font.Design.default))
                     .foregroundColor(self.textColor)
                     .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 10))
                 Spacer()
-                Text(price).font(Font.system(size: 15))
-                    .foregroundColor(self.textColor)
-                .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 10))
+                VStack(alignment: .trailing, spacing: 4) {
+                    if self.paymentDayNear {
+                        Text(price)
+                            .font(Font.system(size: 18, weight: Font.Weight.bold, design: Font.Design.default))
+                            .foregroundColor(self.textColor)
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 10))
+                        Text(self.daysToPaymentText)
+                            .font(Font.system(size: 12, weight: Font.Weight.medium, design: Font.Design.default))
+                            .foregroundColor(self.textColor)
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 10))
+                    } else {
+                        Text(price)
+                            .font(Font.system(size: 18, weight: Font.Weight.bold, design: Font.Design.default))
+                            .foregroundColor(self.textColor)
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 10))
+                    }
                 }
+            }
             .padding(EdgeInsets(top: 15, leading: 10, bottom: 15, trailing: 10))
+            .frame(width: self.widthValue, height: 62, alignment: .leading)
             .background(self.backgroundColor)
             .cornerRadius(6)
             .overlay(
@@ -74,13 +145,38 @@ struct SubscriptionRow_Previews: PreviewProvider {
     static let subscriptionsViewModel = SubscriptionsViewModel()
     
     static var previews: some View {
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-        let subscription: Subscription = Subscription(context: context)
-        subscription.name = "Test"
-        subscription.price = 9
         
-        return SubscriptionRow(subscription: subscription).environmentObject(subscriptionsViewModel)
-            .previewLayout(PreviewLayout.sizeThatFits)
-            .padding()
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        // First Subscription.
+        let subscription1: Subscription = Subscription(context: context)
+        subscription1.id = UUID()
+        subscription1.name = "Test 1"
+        subscription1.price = 9
+        subscription1.cycle = "1y"
+        subscription1.nextPayment = Date()
+        
+        // Second Subscription with a DateComponent.
+        var dayComponent = DateComponents()
+        dayComponent.day = 16 // 2 Weeks
+        
+        let subscription2: Subscription = Subscription(context: context)
+        subscription2.id = UUID()
+        subscription2.name = "Test 2"
+        subscription2.price = 9
+        subscription2.cycle = "2m"
+        subscription2.nextPayment = Calendar.current.date(byAdding: dayComponent, to: Date())!
+        
+        return Group {
+            SubscriptionRow(subscription: subscription1).environmentObject(subscriptionsViewModel)
+                .previewLayout(PreviewLayout.sizeThatFits)
+                .padding()
+                .previewDisplayName("Payment day near")
+            
+            SubscriptionRow(subscription: subscription2).environmentObject(subscriptionsViewModel)
+                .previewLayout(PreviewLayout.sizeThatFits)
+                .padding()
+                .previewDisplayName("Payment day NOT near")
+        }
     }
 }
