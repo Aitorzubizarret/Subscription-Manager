4e6b4d9743fab7ecb9f75b219a53df1fec5154ba
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2020-12-21 23:40:19 +0100

    Fixed the creation of new Subscriptions in 'NewSubscriptionForm'. All subscriptions will have the category 'music' until there is a proper way to choose the category.

--- SubscriptionManager/Views/Subscriptions/Form/NewSubscriptionForm.swift
+++ SubscriptionManager/Views/Subscriptions/Form/NewSubscriptionForm.swift
@@ -43,7 +43,7 @@ struct NewSubscriptionForm: View {
         let selectedRowColor: SubscriptionsViewModel.subscriptionRowColor = SubscriptionsViewModel.subscriptionRowColor.allCases.randomElement() ?? SubscriptionsViewModel.subscriptionRowColor.pistachio
         
         // Save the new subscription in Core Data.
-        self.subscriptionsViewModel.createNewSubscription(name: self.textFieldSubscriptionName, price: formattedSubscriptionPrice, cycle: cycle, nextPayment: self.subscriptionNextPaymentDate, rowColor: selectedRowColor)
+        self.subscriptionsViewModel.createNewSubscription(name: self.textFieldSubscriptionName, price: formattedSubscriptionPrice, cycle: cycle, nextPayment: self.subscriptionNextPaymentDate, rowColor: selectedRowColor, category: .music)
         
         return true
     }
